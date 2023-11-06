3446ee9792f16e27fa1c2e3605d5f2b4c0964939
fmontallana <78574637+fmontallana@users.noreply.github.com>
2023-11-06 08:49:43 +0800

    adjust generated posting

--- src/components/TaskSummary.jsx
+++ src/components/TaskSummary.jsx
@@ -10,11 +10,9 @@ function TaskSummary({ todos }) {
 
   return (
     <div className="flex-1 h-full w-full p-4 lg:p-10 bg-base-200 rounded-lg overflow-y-auto">
-      <p>Good {getDayTime()} po!</p>
-      <p>{new Date().toLocaleString().split(",")[0]}</p>
-      <br />
+      Good {getDayTime()} po! <br />
+      {new Date().toLocaleString().split(",")[0]} <br />
       <p className="font-bold">TASKS</p>
-      <br />
       {todos.length <= 0 && <p>--- No available task ---</p>}
       {filteredClients.map((client) => {
         return (
@@ -23,16 +21,15 @@ function TaskSummary({ todos }) {
             {todos.map((todo) => {
               if (todo.client !== client) return;
               return (
-                <p key={todo.id}>
-                  • {todo.todo} ({todo.status == 0 ? "ongoing" : "done"})
-                </p>
+                <>
+                  • {todo.todo} ({todo.status == 0 ? "ongoing" : "done"}) <br />
+                </>
               );
             })}
+            <br />
           </div>
         );
       })}
-
-      <br />
       <p>Thank you po.</p>
     </div>
   );
